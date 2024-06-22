3a6d092e4070ea2923b7e5b5d7da49faa4e40af8
Andrey Sokolov <traner1945@list.ru>
2024-06-23 00:28:18 +0300

    added gate opening function

--- new.py
+++ new.py
@@ -3,6 +3,9 @@ from colorama import init, Fore
 
 init(autoreset=True)
 
+gate_1 = 0
+gate_2 = 0
+
 
 red_light_first = 0
 green_light_first = 0
@@ -21,6 +24,10 @@ def display_lights_first():
         print(Fore.GREEN + "Зелёный для коров")
     if yellow_light_first:
         print(Fore.YELLOW + "Жёлтый")
+    if gate_1 :
+        print(Fore.GREEN + "Ворота для коров открыты")
+    else:
+        print(Fore.RED + "Ворота для коров закрыты")
 
 # Отображение состояния второго светофора
 def display_lights_second():
@@ -30,11 +37,14 @@ def display_lights_second():
         print(Fore.GREEN + "Зелёный для овец")
     if yellow_light_second:
         print(Fore.YELLOW + "Жёлтый")
-
+    if gate_2:
+        print(Fore.GREEN + "Ворота для овец открыты")
+    else:
+        print(Fore.RED + "Ворота для овец закрыты")
 
 def traffic_lights():
-    global red_light_first, green_light_first, yellow_light_first
-    global red_light_second, green_light_second, yellow_light_second
+    global red_light_first, green_light_first, yellow_light_first, gate_1
+    global red_light_second, green_light_second, yellow_light_second, gate_2
 
     while True:
         red_light_first = 1
@@ -43,6 +53,9 @@ def traffic_lights():
         red_light_second = 0
         green_light_second = 1
         yellow_light_second = 0
+
+        gate_1 = 0
+        gate_2 = 1
         display_lights_first()
         display_lights_second()
         time.sleep(15)
@@ -54,6 +67,8 @@ def traffic_lights():
         red_light_second = 0
         green_light_second = 0
         yellow_light_second = 1
+        gate_1 = 0          #
+        gate_2 = 1          #
         display_lights_first()
         display_lights_second()
         time.sleep(5)
@@ -65,6 +80,9 @@ def traffic_lights():
         red_light_second = 1
         green_light_second = 0
         yellow_light_second = 0
+
+        gate_1 = 1      #
+        gate_2 = 0      #
         display_lights_first()
         display_lights_second()
         time.sleep(15)
@@ -76,12 +94,14 @@ def traffic_lights():
         red_light_second = 0
         green_light_second = 0
         yellow_light_second = 1
+        gate_1 = 1
+        gate_2 = 0
         display_lights_first()
         display_lights_second()
         time.sleep(5)
 
 
-"""Значения переменных: "0" - секция светофора выключена, "1" - секция светофора включена"""
+"""Значения переменных: "0" - секция светофора выключена (ворота закрыты), "1" - секция светофора включена (ворота открыты)"""
 
 if __name__ == "__main__":
     traffic_lights()
